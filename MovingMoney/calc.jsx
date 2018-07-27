cfa81e9db7cbfdf0180ee4b1c7fccedf0760daa0
Jo-Ann <joannjefthas@gmail.com>
2018-07-27 15:38:41 +0200

    second try

---
+++ MovingMoney/calc.jsx
@@ -0,0 +1,14 @@
+class Form extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1> I Am Awesome</h1>
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(
+    <Form />,
+    document.getElementById('sample')
+)
